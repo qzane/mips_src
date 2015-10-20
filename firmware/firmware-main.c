2aa222d730a071c301bd07c091f90f0bdf23a584
qzane <qzane@live.com>
2015-10-21 00:39:09 +0800

    ROM and RAM

--- firmware/firmware-main.c
+++ firmware/firmware-main.c
@@ -3,14 +3,33 @@
 #include "stdarg.h"
 #include "stdio.h"
 
+extern char __rodata_end__[];
+extern char __data_begin__[],__data_end__[];
+extern char __bss_begin__[],__bss_end__[];
+
+void reloc_data_bss(void){
+   memcpy(__data_begin__, __rodata_end__, __data_end__ - __data_begin__);
+   memset(__bss_begin__, 0, __bss_end__ - __bss_begin__);
+}
+
 int kprintf(char *fmt,...);
 void kputs(const char *buf);
 
+int g = 6;
+
+void change_global_data(void){
+   kprintf("g = %d\n",g);
+   kprintf("addr g = %x\n",&g);
+   g = 8;
+   kprintf("g = %d\n",g);
+}
 
 int main(void){
     int a=34;
-    //while(1)
-    kputs("Hello there!");
+   //while(1)
+    reloc_data_bss();
+    kputs("Hello there!\n");
+    change_global_data();
     kprintf("One: %d Hex of 34: 0x%x done\n",1,a);
     while(1);/* endless loop */
     return 0;
