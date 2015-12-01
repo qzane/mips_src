db3d959a3eaf81d160b1a78668ef10fcfe526824
unknown <qzane@live.com>
2015-12-01 17:02:36 +0800

    spinlock_lock

---
+++ include/arch/mips/asm/spinlock.h
@@ -0,0 +1,38 @@
+#ifndef _ASM_SPINLOCK_H
+#define _ASM_SPINLOCK_H
+
+#include <sys/types.h>
+#include <asm/atomic.h>
+
+
+/* ... */
+struct spinlock {
+        uint32_t        state;
+#define LOCK_BIT        0
+};
+/* ... */
+
+
+static void spinlock_lock(struct spinlock *lock)
+{
+	bool result;
+	uint32_t bit;
+	asm volatile (
+		"1:	ll	%[reg], %[mem];"
+		"   and %[reg1], %[reg], %[val]"
+		"   bne %[reg1], 1b;"
+		"	or	%[reg], %[val];"
+		"	sc	%[reg], %[mem];"
+		"	beqz	%[reg], 1b;"
+		: [reg]"=&r"(result), [mem]"+m"(*addr), [reg1]"=&r"(bit)
+		: [val]"ir"(1 << LOCK_BIT)
+	);
+}
+static void spinlock_unlock(struct spinlock *lock)
+{
+	;
+}
+
+
+
+#endif
